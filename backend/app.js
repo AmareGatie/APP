567efc02e49cc1171149efaa932664fbb46e640d
AmareGatie <amaregatie2006@gmail.com>
2024-02-22 17:01:39 +0300

    Commite

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 //import the express module
 const express =require("express");
 // set up the port to listen 
-
-
-
-
-
+const port =4001
+// set Up listner
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
